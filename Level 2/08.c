707ea22a0f4be6e906cec9028915963ae8cab581
Md Tanjil Ahammed <zarcanima@gmail.com>
2026-07-10 04:37:54 +0600

    Create 08.c

---
+++ Level 2/08.c
@@ -0,0 +1,11 @@
+/*Program that will take two numbers as inputs and print the maximum value. (Using 
+conditional operator - ?)*/
+#include<stdio.h>
+int main(){
+int a,b,max;
+printf("Enter 2 number : ");
+scanf("%d%d",&a,&b);
+max= (a>b) ? a:b;
+printf("Max : %d\n",max);
+return 0;
+}
