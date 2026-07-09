44781fbcb19e44580c01b37036d3c2f02ff0eb90
Md Tanjil Ahammed <zarcanima@gmail.com>
2026-07-10 05:51:03 +0600

    Update 14.c

--- Level 2/14.c
+++ Level 2/14.c
@@ -2,11 +2,13 @@
 */
 #include<stdio.h>
 int main(){
-int a=4;
-float b=4.89;
-char ch='c';
-printf("Size of int : %zu \n",sizeof(a));
+int a;
+float b;
+char ch;
+double d;
+printf(" Size of int : %zu \n",sizeof(a));
 printf(" Size of float : %zu \n",sizeof(b));
 printf(" Size of char : %zu \n",sizeof(ch));
+printf(" Size of double : %zu \n",sizeof(d));
 return 0;
 }
