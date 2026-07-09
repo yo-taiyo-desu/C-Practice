28561a15a980e4d8d94e919e26a8bb5241df8b2e
Md Tanjil Ahammed <zarcanima@gmail.com>
2026-07-09 23:37:43 +0600

    Create 13.c

---
+++ Level 01/13.c
@@ -0,0 +1,9 @@
+/*Program that will define a constant using “DEFINE” and print the value.
+*/
+#include<stdio.h>
+#define value 100
+int main(){
+  printf("The value is : %d\n",value);
+return 0;
+}
+  
