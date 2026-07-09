3c793d69076a3c584312158f2c37846ddb8204bb
Md Tanjil Ahammed <zarcanima@gmail.com>
2026-07-09 22:38:37 +0600

    Create 06.c

---
+++ Level 01/06.c
@@ -0,0 +1,10 @@
+/*Program that will take your age in year(s) as input and print it.
+*/
+#include<stdio.h>
+int main(){
+  int age;
+printf("Enter your age : ");
+scanf("%d",&age);
+printf("Your age is : %d",age);
+return 0;
+}
