729862475e320399e03efca9d737d1350e02907e
Md Tanjil Ahammed <zarcanima@gmail.com>
2026-07-09 23:34:56 +0600

    Create 12.c

---
+++ Level 01/12.c
@@ -0,0 +1,8 @@
+/*Program that will define a constant using “CONST” and print the value.
+*/
+#include<stdio.h>
+int main(){
+  const double pi=3.14167;
+printf("The value is : %f\n",pi);
+return 0;
+}
