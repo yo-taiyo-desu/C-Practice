bf80419037f54b6bfa073c6642d7319fabf5b8e9
Md Tanjil Ahammed <zarcanima@gmail.com>
2026-07-10 04:42:53 +0600

    Create 09.c

---
+++ Level 2/09.c
@@ -0,0 +1,17 @@
+/*Program that will evaluate the following equations -
+X = a – b / 3 + c * 2 – 1
+Y = a – ( b / ( 3 + c ) * 2) - 1 
+Z = a – ( ( b / 3) + c * 2) - 1 */
+#include<stdio.h>
+int main(){
+int a,b,c,X,Y,Z;
+printf("Enter the numbers : ");
+scanf("%d%d%d",&a,&b,&c);
+X=(a-b)/(3+c)*(2-1);
+Y=a-(b/(3+c)*2)-1;
+Z=a-((b/3)+c*2)-1;
+printf("X = %d\n",X);
+printf("Y = %d\n",Y);
+printf("Z = %d\n",Z);
+return 0;
+}
