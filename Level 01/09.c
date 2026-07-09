f7a96633e16b6eda2cb3f34edfb47396cc869f0f
Md Tanjil Ahammed <zarcanima@gmail.com>
2026-07-09 23:02:29 +0600

    Create 09.c

---
+++ Level 01/09.c
@@ -0,0 +1,11 @@
+/*Program that will declare a variable from each data type: double, boolean. Then it will 
+initialize them with values and print them.*/
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+  double a=5.629476362847;
+bool isfalse=true;
+printf("The double value : %lf\n",a);
+printf("The boolean value : %d\n",isfalse);
+return 0;
+}
