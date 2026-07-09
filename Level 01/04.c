df1c06f0b4854c6268e4c9df872de191b0d3ca05
Md Tanjil Ahammed <zarcanima@gmail.com>
2026-07-09 20:41:09 +0600

    Create 04.c

---
+++ Level 01/04.c
@@ -0,0 +1,12 @@
+/*Program that will declare an integer, a floating point number, a character. Then it will 
+initialize them with values and print those values*/
+#include<stdio.h>
+int main(){
+int a=1;
+float b=1.567;
+char ch='a';
+printf("The integer value : %d\n",a);
+printf("The floating point value : %f\n",b);
+printf("The character value : %c\n",ch);
+return 0;
+}
