afbbe67bb0952809eb367b96dbae4e69dbbe6cec
Md Tanjil Ahammed <zarcanima@gmail.com>
2026-07-10 00:04:57 +0600

    Update 01.c

--- Level 2/01.c
+++ Level 2/01.c
@@ -1,6 +1,7 @@
 /*Program that will take two numbers X and Y as inputs, then calculate and print the values 
 of their addition, subtraction, multiplication, division (quotient and reminder).*/
 #include<stdio.h>
+#include<math.h>
 int main(){
 float a,b;
 printf("Enter two numbers : ");
@@ -13,7 +14,7 @@ printf("2) Subtraction : %f\n",a-b);
 printf("3) Multiplication : %f\n",a*b);
 //a/b & a%b
 printf("4) Division : \n");
-printf("1) Quotient : %f\n",a%b);
-printf("2) Remainder : %f\n",a/b);
+printf("1) Quotient : %f\n",a/b);
+printf("2) Remainder : %f\n",fmod(a,b));
 return 0;
 }
