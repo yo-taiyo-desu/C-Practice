b6dcf4d1c7e19060de65f2d24f96437d45b85065
Md Tanjil Ahammed <zarcanima@gmail.com>
2026-07-10 08:37:52 +0600

    Create 03.c

---
+++ Level 4/03.c
@@ -0,0 +1,20 @@
+/*Write a program (WAP) that will print following series upto Nth terms.
+1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, …….*/
+#include<stdio.h>
+int main(){
+int n,i;
+printf("Series upto : ");
+scanf("%d",&n);
+for(i=1;i<=n;i++){
+if(i%2==0){
+printf("0");
+}
+else{
+printf("1");
+}
+if(i<n){
+printf(" , ");
+}
+}
+return 0;
+}
