68d5ed2ce28c680142b1d45844b92ffd2240eae8
Md Tanjil Ahammed <zarcanima@gmail.com>
2026-07-10 07:10:41 +0600

    Create 08.c

---
+++ Level 3/08.c
@@ -0,0 +1,14 @@
+/*Program that will decide whether a year is leap year or not 
+*/
+#include<stdio.h>
+int main(){
+int year;
+printf("Enter the year : ");
+scanf("%d",&year);
+if((year%400==0)||(year%4==0&&year%100!=0)){
+printf("%d is a leap year \n",year);
+}
+else
+printf("%d is not a leap year\n",year);
+return 0;
+}
