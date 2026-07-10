cd4256150319cbcdf86ee9fb2f30c1b224e68e5b
Md Tanjil Ahammed <zarcanima@gmail.com>
2026-07-10 06:23:57 +0600

    Create 02.c

---
+++ Level 3/02.c
@@ -0,0 +1,18 @@
+/*Program that will decide whether a number is even or odd
+*/
+#include<stdio.h>
+int main(){
+double a;
+printf("Enter a number : ");
+scanf("%lf",&a);
+if(a%2==0){
+printf("%lf is positive \n",a);
+}
+else if(a%2!=0){
+printf("%lf is negative \n",a);
+}
+else{
+printf("%lf is Zero \n",a);
+}
+return 0;
+}
