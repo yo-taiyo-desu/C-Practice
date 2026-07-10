569bc15760dc776f76b86fd26fb3ab8d93f5753b
Md Tanjil Ahammed <zarcanima@gmail.com>
2026-07-10 10:23:42 +0600

    Create 15.c

---
+++ Level 4/15.c
@@ -0,0 +1,20 @@
+/*WAP that will find the GCD (greatest common divisor) and LCM (least common multiple) 
+of two positive integers*/
+#include<stdio.h>
+int main(){
+int n1,n2,i,gcd,lcm,temp,r,num2,num1;
+printf("Enter 2 numbers : ");
+scanf("%d%d",&n1,&n2);
+num1=n1;
+num2=n2;
+while(num2!=0){
+r=num1%num2;
+num1=num2;
+num2=r;
+}
+gcd=num1;
+lcm=(n1*n2)/gcd;
+printf("GCD is : %d\n",gcd);
+printf("LCM is : %d\n",lcm);
+return 0;
+}
