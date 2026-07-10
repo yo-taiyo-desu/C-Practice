adb0452430820d0afe173dbb68d1217c279fda4c
Md Tanjil Ahammed <zarcanima@gmail.com>
2026-07-10 06:00:08 +0600

    Update 01.c

--- Level 3/01.c
+++ Level 3/01.c
@@ -1,1 +1,17 @@
-
+/*Program that will decide whether a number is positive or not.*/
+#include<stdio.h>
+int main(){
+double a;
+printf("Enter a number : ");
+scanf("%lf",&a);
+if(a>0){
+printf("%lf is positive \n",a);
+}
+else if(a<0){
+printf("%lf is negative \n",a);
+}
+else{
+printf("%lf is Zero \n",a);
+}
+return 0;
+}
