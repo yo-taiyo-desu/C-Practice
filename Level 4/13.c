7c8cea6c757ee2c96b42c819f734505a5794cdf8
Md Tanjil Ahammed <zarcanima@gmail.com>
2026-07-10 10:05:42 +0600

    Create 13.c

---
+++ Level 4/13.c
@@ -0,0 +1,25 @@
+/*Write a program (WAP) that will find nCr where n >= r; n and r are integers 
+*/
+#include<stdio.h>
+int main(){
+int n,r,i,ncr,factn=1,factr=1,factnr=1;
+printf("Enter n : ");
+scanf("%d",&n);
+printf("Enter r : ");
+if(r>n){
+printf("Invalid");
+}
+scanf("%d",&r);
+for(i=1;i<=n;i++){
+factn=factn*i;
+}
+for(i=1;i<=r;i++){
+factr=factr*i;
+}
+for(i=1;i<=(n-r);i++){
+factnr=factnr*i;
+}
+ncr=factn/(factr*factnr);
+printf("NCR of %d & %d is : %d\n",n,r,ncr);
+return 0;
+}
